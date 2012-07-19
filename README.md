e7a3e193a2ceee85f0d500cbb5c23ad94a86c995
jbbarth <jeanbaptiste.barth@gmail.com>
2012-07-19 13:16:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Redmine Refresh plugin
+======================
+
+This plugins adds an automatic refresh on issue lists.
---
+++ test/test_helper.rb
@@ -0,0 +1,2 @@
+# Load the normal Rails helper
+require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
