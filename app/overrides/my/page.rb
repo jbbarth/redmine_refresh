fcdcc7d2d6037ef9d37e92274e65d98ccb5fe381
jbbarth <jeanbaptiste.barth@gmail.com>
2012-08-29 13:54:23 +0200

    Added automatic refresh option in my/page

---
+++ app/overrides/my/page.rb
@@ -0,0 +1,4 @@
+Deface::Override.new :virtual_path  => 'my/page',
+                     :name          => 'add-refresh-link-to-my-page',
+                     :insert_top    => 'div.contextual',
+                     :partial       => 'redmine_refresh/refresher'
