cbe4be56541041907f8b184b8032e3c5e766cc5d
jbbarth <jeanbaptiste.barth@gmail.com>
2012-09-08 22:16:09 +0200

    Added original partial fingerprints to deface overrides

--- app/overrides/issues/index.rb
+++ app/overrides/issues/index.rb
@@ -1,4 +1,5 @@
 Deface::Override.new :virtual_path  => 'issues/index',
+                     :original      => '402b3096154c5706049c1de2f9fedeaa63f6b618',
                      :name          => 'add-refresh-link-to-issues-list',
                      :insert_top    => 'div.contextual',
                      :partial       => 'redmine_refresh/refresher'
--- app/overrides/my/page.rb
+++ app/overrides/my/page.rb
@@ -1,4 +1,5 @@
 Deface::Override.new :virtual_path  => 'my/page',
+                     :original      => '7b02b984dabd756a62959aeea9c05db9d378bf7a',
                      :name          => 'add-refresh-link-to-my-page',
                      :insert_top    => 'div.contextual',
                      :partial       => 'redmine_refresh/refresher'
