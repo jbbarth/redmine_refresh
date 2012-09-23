ff9cc8b35209093ca7fa1716956170cde1d0d934
jbbarth <jeanbaptiste.barth@gmail.com>
2012-09-24 00:36:18 +0200

    Switch to jQuery and redmine 2.1.0

--- init.rb
+++ init.rb
@@ -12,7 +12,7 @@ Redmine::Plugin.register :redmine_refresh do
   author 'Jean-Baptiste BARTH'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
   url 'http://github.com/jbbarth/redmine_refresh'
-  version '0.0.1'
-  requires_redmine :version_or_higher => '2.0.0'
+  version '0.2.0'
+  requires_redmine :version_or_higher => '2.1.0'
   #settings :default => { }, :partial => 'settings/sudo_settings'
 end
