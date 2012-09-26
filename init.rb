8ff95117d9edf6e14405c57e4d030cf8fb6fde4b
jbbarth <jeanbaptiste.barth@gmail.com>
2012-09-26 13:48:46 +0200

    Consistency with my other plugins

--- init.rb
+++ init.rb
@@ -9,9 +9,9 @@ Rails.application.paths["app/overrides"] << File.expand_path("../app/overrides",
 Redmine::Plugin.register :redmine_refresh do
   name 'Redmine Refresh plugin'
   description 'This is a plugin for Redmine'
+  url 'https://github.com/jbbarth/redmine_refresh'
   author 'Jean-Baptiste BARTH'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
-  url 'http://github.com/jbbarth/redmine_refresh'
   version '0.2.0'
   requires_redmine :version_or_higher => '2.1.0'
   #settings :default => { }, :partial => 'settings/sudo_settings'
