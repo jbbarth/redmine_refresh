a157b2b9efdb60847e704d0a427bf2d24fc23484
jbbarth <jeanbaptiste.barth@gmail.com>
2013-05-03 18:56:56 +0200

    Bump to version 0.3.0

--- init.rb
+++ init.rb
@@ -12,7 +12,8 @@ Redmine::Plugin.register :redmine_refresh do
   url 'https://github.com/jbbarth/redmine_refresh'
   author 'Jean-Baptiste BARTH'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
-  version '0.2.0'
+  #TODO: make it depend on redmine_refresh/version
+  version '0.3.0'
   requires_redmine :version_or_higher => '2.1.0'
   #settings :default => { }, :partial => 'settings/sudo_settings'
 end
