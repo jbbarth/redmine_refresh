c4b814c0755638d44df4df8290a6e25629f58d99
jbbarth <jeanbaptiste.barth@gmail.com>
2013-05-03 19:13:29 +0200

    Make version in init.rb depend on redmine_refresh/version.rb

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
+require "redmine_refresh/version"
+
 Redmine::Plugin.register :redmine_refresh do
   name 'Redmine Refresh plugin'
   description 'This is a plugin for Redmine'
@@ -5,7 +7,7 @@ Redmine::Plugin.register :redmine_refresh do
   author 'Jean-Baptiste BARTH'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
   #TODO: make it depend on redmine_refresh/version
-  version '0.3.0'
+  version RedmineRefresh::VERSION
   requires_redmine :version_or_higher => '2.1.0'
   #settings :default => { }, :partial => 'settings/sudo_settings'
 end
