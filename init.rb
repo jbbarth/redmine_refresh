38dae218e564c9f831d0391e64a245422beefc38
jbbarth <jeanbaptiste.barth@gmail.com>
2013-05-03 19:10:57 +0200

    Remove outdated hack to integrate deface overrides paths (automatic now that our plugin is an Engine)

--- init.rb
+++ init.rb
@@ -1,11 +1,3 @@
-# Little hack for using the 'deface' gem in redmine:
-# - redmine plugins are not railties nor engines, so deface overrides in app/overrides/ are not detected automatically
-# - deface doesn't support direct loading anymore ; it unloads everything at boot so that reload in dev works
-# - hack consists in adding "app/overrides" path of the plugin in Redmine's main #paths
-# TODO: see if it's complicated to turn a plugin into a Railtie or find something a bit cleaner
-Rails.application.paths["app/overrides"] ||= []
-Rails.application.paths["app/overrides"] << File.expand_path("../app/overrides", __FILE__)
-
 Redmine::Plugin.register :redmine_refresh do
   name 'Redmine Refresh plugin'
   description 'This is a plugin for Redmine'
