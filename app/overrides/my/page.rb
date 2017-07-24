eac612a17499c18a4088b1f40256532be878c86b
Vincent Robert <vincent.robert@nanego.com>
2017-07-24 12:04:01 +0200

    Temporarily add override to custom page

--- app/overrides/my/page.rb
+++ app/overrides/my/page.rb
@@ -3,3 +3,9 @@ Deface::Override.new :virtual_path  => 'my/page',
                      :name          => 'add-refresh-link-to-my-page',
                      :insert_top    => 'div.contextual',
                      :partial       => 'redmine_refresh/refresher'
+
+Deface::Override.new :virtual_path  => 'my/custom_page',
+                     :original      => '7b02b984dabd756a62959aeea9c05db9d378bf7a',
+                     :name          => 'add-refresh-link-to-my-custom-page',
+                     :insert_top    => 'div.contextual',
+                     :partial       => 'redmine_refresh/refresher'
