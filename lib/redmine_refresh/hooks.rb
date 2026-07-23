7b09a723803f6998314100b418e66164b645be9d
Vincent <contact@vincent-robert.com>
2026-07-23 15:51:08 +0200

    Remove obsolete non-breaking space

--- lib/redmine_refresh/hooks.rb
+++ lib/redmine_refresh/hooks.rb
@@ -1,11 +1,5 @@
 module RedmineRefresh
 
-  class Hooks < Redmine::Hook::ViewListener
-    def view_layouts_base_html_head(context)
-      stylesheet_link_tag("redmine_refresh", :plugin => "redmine_refresh")
-    end
-  end
-
   class ModelHook < Redmine::Hook::Listener
     def after_plugins_loaded(_context = {})
       require_relative '../redmine_refresh'
